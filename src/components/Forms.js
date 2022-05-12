1e22b9b3bedec35feb16b8b827008e5e2201587c
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-05-12 16:15:24 -0300

    Correção lint do css e requisito 2 sem o hasTriunfo

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,52 +1,106 @@
 import React from 'react';
 import '../App.css';
+import PropTypes from 'prop-types';
 
 class Forms extends React.Component {
   render() {
+    const { cardName, cardDescription, cardAttr1 } = this.props;
+    const { cardAttr2, cardAttr3, cardImage } = this.props;
+    const { cardRare, cardTrunfo } = this.props;
+    const { isSaveButtonDisabled, onInputChange, onSaveButtonClick } = this.props;
     return (
       <div className="form">
-        <div className="div-form">
-          {/* <label for="name">Insira um nome para a carta: </label> */}
-          <input type="text" id="name" data-testid="name-input" />
-        </div>
-        <div className="div-form">
-          {/* <label for="description">Insira uma descrição:</label> */}
-          <p><textarea id="description" data-testid="description-input" /></p>
-        </div>
-        <div className="div-form">
-          {/* <label for="firstStatus">Attr01:</label> */}
-          <input id="firsttatus" type="number" data-testid="attr1-input" />
-        </div>
-        <div className="div-form">
-          {/* <label for="secondStatus">Attr02:</label> */}
-          <input id="secondStatus" type="number" data-testid="attr2-input" />
-        </div>
-        <div className="div-form">
-          {/* <label for="thirdStatus">Attr03:</label> */}
-          <input id="thirdStatus" type="number" data-testid="attr3-input" />
-        </div>
-        <div className="div-form">
-          {/* <label for="linkImage">Imagem:</label> */}
-          <input id="linkImage" type="text" data-testid="image-input" />
-        </div>
-        <div className="div-form">
-          {/* <label for="rare-rank">Raridade:</label> */}
-          <select id="rare-rank" data-testid="rare-input">
-            <option value="normal">normal</option>
-            <option value="raro">raro</option>
-            <option value="muito raro">muito raro</option>
-          </select>
-        </div>
-        <div className="div-form">
-          <input id="super-trunfo" type="checkbox" data-testid="trunfo-input" />
-          {/* <label for="super-trunfo">Super-Trunfo</label> */}
-        </div>
-        <div className="div-form">
-          <button type="button" data-testid="save-button">Salvar</button>
-        </div>
+        <p>Insira um nome para a carta: </p>
+        <input
+          type="text"
+          value={ cardName }
+          onChange={ onInputChange }
+          data-testid="name-input"
+        />
+        <p>Insira uma descrição:</p>
+        <textarea
+          value={ cardDescription }
+          onChange={ onInputChange }
+          data-testid="description-input"
+        />
+        <p>Attr01:</p>
+        <input
+          id="firstat"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
+          type="number"
+          data-testid="attr1-input"
+        />
+        <p>Attr02:</p>
+        <input
+          id="secStat"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
+          type="number"
+          data-testid="attr2-input"
+        />
+        <p>Attr03</p>
+        <input
+          id="thiStat"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
+          type="number"
+          data-testid="attr3-input"
+        />
+        <p>Imagem:</p>
+        <input
+          id="linkImage"
+          type="text"
+          value={ cardImage }
+          onChange={ onInputChange }
+          data-testid="image-input"
+        />
+        <p>Raridade:</p>
+        <select
+          id="rare-rank"
+          value={ cardRare }
+          onChange={ onInputChange }
+          data-testid="rare-input"
+        >
+          <option value="normal">normal</option>
+          <option value="raro">raro</option>
+          <option value="muito raro">muito raro</option>
+        </select>
+        <br />
+        <input
+          id="super-trunfo"
+          type="checkbox"
+          checked={ cardTrunfo }
+          onChange={ onInputChange }
+          data-testid="trunfo-input"
+        />
+        <span>Super-Trunfo</span>
+        <br />
+        <button
+          type="button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
       </div>
     );
   }
 }
+
+Forms.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Forms;
