95e104b42536b6c986832db7b23cbaac97a3d74d
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-05-12 20:13:23 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ class App extends React.Component {
     cardTrunfo: false,
     hasTrunfo: false,
     isSaveButtonDisabled: false,
-    // onInputChange, uma callback;
-    // onSaveButtonClick
   }
 
   onInputChange = ({ target }) => {
@@ -26,26 +24,33 @@ class App extends React.Component {
 
   render() {
     console.log(this.state);
-    const { cardName, cardDescription, cardAttr1, cardAttr2, cardRare } = this.state;
-    const { cardAttr3, cardImage, cardTrunfo } = this.state;
-    const { hasTrunfo, isSaveButtonDisabled } = this.state;
+    const { cardName: cdGame, cardDescription: cdDescrip } = this.state;
+    const { cardAttr1, cardAttr2, cardRare } = this.state;
+    const { cardAttr3, cardImage: cdImg, cardTrunfo } = this.state;
+    const { hasTrunfo, isSaveButtonDisabled: iSaveBtn } = this.state;
     console.log(hasTrunfo);
-    console.log(isSaveButtonDisabled);
+
+    if (cdGame === '' || cdDescrip === '' || cdImg === '' || cardRare === '') {
+      this.setState({ iSaveBtn: true });
+    } else {
+      this.setState({ iSaveBtn: false });
+    }
+
     return (
       <div className="principal">
         <h1>Tryunfo</h1>
         <div className="form-card">
           <div className="form-conteiner-Component">
-            <Forms onInputChange={ this.onInputChange } />
+            <Forms isSaveButton={ iSaveBtn } onInputChange={ this.onInputChange } />
           </div>
           <div className="card-conteiner-Component">
             <Card
-              cardName={ cardName }
-              cardDescription={ cardDescription }
+              cardName={ cdGame }
+              cardDescription={ cdDescrip }
               cardAttr1={ cardAttr1 }
               cardAttr2={ cardAttr2 }
               cardAttr3={ cardAttr3 }
-              cardImage={ cardImage }
+              cardImage={ cdImg }
               cardRare={ cardRare }
               cardTrunfo={ cardTrunfo }
             />
